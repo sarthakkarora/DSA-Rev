e74760796e573a6f60940635698f6fca45867427
Sarthak Arora <130503783+sarthakkarora@users.noreply.github.com>
2024-07-25 23:29:58 +0530

    ArrayExtra1.java

--- ArrayExtra1.java
+++ ArrayExtra1.java
@@ -1,12 +1,13 @@
+// Reverse
 import java.util.Arrays;
 
-public class Array6 {
+public class ArrayExtra1 {
     public static void main(String[] args) {
         int[] arr = {11, 7, 3, 12, 4};
         int i = 0;
         int j = arr.length - 1;
 
-        // Reverse the array
+        
         while (i < j) {
             int temp = arr[i];
             arr[i] = arr[j];
@@ -15,7 +16,7 @@ public class Array6 {
             j--;
         }
 
-        // Print the reversed array twice
+        
         System.out.println(Arrays.toString(arr));
         
     }
