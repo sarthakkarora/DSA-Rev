2e13f8836d66d87d4a4a3c9600521cade12298db
Sarthak Arora <130503783+sarthakkarora@users.noreply.github.com>
2024-07-25 22:19:16 +0530

    Add files via upload

---
+++ Array7.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Array7{
+    public static void main(String[] args) {
+        
+        int[] a={1,4,9,2};
+
+        int[] b={3,7,6};
+
+        int[] c= new int[a.length + b.length];
+
+        for(int i=0;i<a.length; i++){
+            c[i]=a[i];
+        }
+
+        for(int i=0; i<b.length; i++){
+            c[i + a.length]= b[i];
+        }
+      
+        Arrays.sort(c);
+System.out.println(Arrays.toString(c));
+    }
+}
